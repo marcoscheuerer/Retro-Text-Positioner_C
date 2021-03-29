c2304a9ec75590b13d1cdcef9f15140ce7e3a0be
Marco Scheuerer <info@marco-scheuerer.com>
2021-03-29 02:00:53 +0200

    Input-problems fixed, mixed color implemented, comments in doxygen included.

--- retro-text-positioner.c
+++ retro-text-positioner.c
@@ -4,6 +4,7 @@
 #include <string.h>
 #include <ctype.h>
 #include <wchar.h>
+#include <time.h>
 #include "my_art.h"
 #include "my_color.h"
 
@@ -26,21 +27,28 @@ const wchar_t* convertToRetroLetter(const wchar_t *ptrLetter, char letter);
 
 enum BOOL { false, true };
 
+/******************************************************************************/
+/* Main Function                                                              */
+/******************************************************************************/
 int main(void)
 {
     setlocale(LC_ALL, "");
     enum BOOL anotherText = true;
     char choice;
 
+    srand(time(0));
+
     char letter,
-         text[20] = DEFAULT_TEXT,
-         color[12] = DEFAULT_COLOR;
-    int xPos = DEFAULT_ROW, 
-        yPos = DEFAULT_COL;
+         num[4],
+         text[20],
+         color[12];
+    int xPos, 
+        yPos;
 
     do
     {
-    
+        strcpy(text, " ");
+        strcpy(color, " ");
         clear();
 
         printf("Funny Retro-Text-Positioner\n"
@@ -52,15 +60,35 @@ int main(void)
         printf("Please choose a color you like %s[default: %s}%s: ", BLUE, DEFAULT_COLOR, DEFAULT);
         fgets(color, 12, stdin);
         printf("Row number to print %s[default: %d]%s: ", BLUE, DEFAULT_ROW, DEFAULT);
-        scanf("%d", &xPos);
+        fgets(num, 4, stdin);
+        //getchar();
+        xPos = atoi(num);
+        //xPos = getchar(); getchar();
         printf("Column number to print %s[default: %d]%s: ", BLUE, DEFAULT_COL, DEFAULT);
-        scanf("%d", &yPos);
+        fgets(num, 4, stdin);
+        //getchar();
+        yPos = atoi(num);
+
+
+        if(text[0] == '\n')
+            strcpy(text, DEFAULT_TEXT);
+        else
+            text[strlen(text)-1] = '\0';
+
+        if(color[0] == '\n')
+            strcpy(color, DEFAULT_COLOR);
+        if(xPos == 0)
+            xPos = DEFAULT_ROW;
+        if(yPos == 0)
+            yPos = DEFAULT_COL;
+
+        //yPos = getchar(); getchar();
         textToLower(color);
 
         //printf(color);
         setText(xPos, yPos, text, color);
 
-        getchar();
+        
         printf("\n\nDo you want to write a new Text [y/n]: ");
         choice = getchar(); getchar();
 
@@ -73,11 +101,23 @@ int main(void)
     } while (anotherText);
 
     clear();
-    printf("\n\nThanks for getting retro, ║ after so many years!\n\n");
+    printf("\n\n\t🚀  Thanks for getting retro, after so many years!  🚀\n\n");
 
     return 0;
 }
-    
+
+/******************************************************************************/
+/* Functions                                                                  */
+/******************************************************************************/    
+
+/**
+ * @brief Prints a retrostyled letter on the console display
+ *
+ * @param xPos x-Position of letter
+ * @param yPos y-Position of letter
+ * @param letter ASCII-letter to convert
+ * @param color Color of retrostyled letter
+ */
 void setLetter(int xPos, int yPos, char letter, char* color)
 {
     const wchar_t *ptrLetter = NULL;
@@ -88,10 +128,13 @@ void setLetter(int xPos, int yPos, char letter, char* color)
     int xPos2=0, yPos2=0;
 
     ptrLetter = convertToRetroLetter(ptrLetter, letter);
+    
     setTextColor(color);
 
     for(i = 0; ptrLetter[i] != '\0'; i++)
     {
+        //setTextColor(color);
+
         element = ptrLetter[i];
 
         if(element == '\n')
@@ -106,35 +149,20 @@ void setLetter(int xPos, int yPos, char letter, char* color)
         printf("\x1b[%d;%dH%lc", xPos2, yPos2, element);
         count += 1;
     }
+
+    fflush(stdout);
+
     resetColor();
 }
 
-void setTextColor(char* color)
-{
-    const char *ptrColor = NULL;
-
-    if(strcmp(color, "black") == 0)
-        ptrColor = BLACK;
-    else if(strcmp(color, "red") == 0)
-        ptrColor = RED;
-    else if(strcmp(color, "green")==0)
-        ptrColor = GREEN;
-    else if(strcmp(color, "yellow")==0)
-        ptrColor = YELLOW;
-    else if(strcmp(color, "blue")==0)
-        ptrColor = BLUE;
-    else if(strcmp(color, "magenta")==0)
-        ptrColor = MAGENTA;
-    else if(strcmp(color, "cyan")==0)
-        ptrColor = CYAN;
-    else if(strcmp(color, "white")==0)
-        ptrColor = WHITE;
-    else
-        ptrColor = DEFAULT;
-
-    printf("%s", ptrColor);
-}
-
+/**
+ * @brief Prints the complete text on the screen
+ * 
+ * @param xPos x-Position of text
+ * @param yPos y-Position of letter
+ * @param text ascii-text to print retrostyled on screen
+ * @param color color of the text
+ */
 void setText(int xPos, int yPos, char *text, char* color)
 {
     int i;
@@ -157,24 +185,80 @@ void setText(int xPos, int yPos, char *text, char* color)
 
         furtherPos += (maxLength + space);
     }
-    
-
 }
 
+/**
+ * @brief Sets the color of the text with defined colors
+ * 
+ * @param color color to pick from a color list
+ */
+void setTextColor(char* color)
+{
+    int randNumber;
+    char letterColor[12] = { '\0' };
+
+    const char *ptrColor = NULL;
+
+    const char colors[][10] = { "black", "red", "green", "yellow", "blue", "magenta", "cyan", "white" };
+
+    strcpy(letterColor, color);
+
+    if(strcmp(color, "mixed") == 0)
+    {
+        randNumber = rand() % 8;
+        strcpy(letterColor, colors[randNumber]);
+    }
+
+    if(strcmp(letterColor, "black") == 0)
+        ptrColor = BLACK;
+    else if(strcmp(letterColor, "red") == 0)
+        ptrColor = RED;
+    else if(strcmp(letterColor, "green") == 0)
+        ptrColor = GREEN;
+    else if(strcmp(letterColor, "yellow") == 0)
+        ptrColor = YELLOW;
+    else if(strcmp(letterColor, "blue") == 0)
+        ptrColor = BLUE;
+    else if(strcmp(letterColor, "magenta") == 0)
+        ptrColor = MAGENTA;
+    else if(strcmp(letterColor, "cyan") == 0)
+        ptrColor = CYAN;
+    else if(strcmp(letterColor, "white") == 0)
+        ptrColor = WHITE;
+    else
+        ptrColor = DEFAULT;
+
+    printf("%s", ptrColor);
+}
+
+/**
+ * @brief Resets the color to the system color
+ * 
+ */
 void resetColor()
 {
     printf("%s", RESET);
 }
 
+/**
+ * @brief Change the complete text to uppercase
+ * 
+ * @param text text to change into upper letters
+ */
 void textToUpper(char *text)
 {
     int i;
 
-    for(i = 0; text[i] != '\n'; i++)
+    for(i = 0; text[i] != '\0'; i++)
         text[i] = toupper(text[i]);
     text[i] = '\0';
 }
 
+/**
+ * @brief Change the complete text to lowercase
+ * 
+ * @param text text to change into lower letters
+ */
 void textToLower(char *text)
 {
     int i;
@@ -184,11 +268,23 @@ void textToLower(char *text)
     text[i] = '\0';
 }
 
+/**
+ * @brief Calculates the complete elements in a retrostyled letter
+ * 
+ * @param retroLetter Retrostyled letter to calculate
+ * @return Returns the complete elements of a resrostyled letter
+ */
 int elementsInRetroLetter(const wchar_t *retroLetter)
 {
     return (int)wcslen(retroLetter);
 }
 
+/**
+ * @brief Calculates the maximum elements in a line of the retrostyled letter
+ * 
+ * @param retroLetter Retrostyled letter to calculate
+ * @return Returns the complete elements of a resrostyled letter
+ */
 int maxWidthRetroLetter(const wchar_t *retroLetter)
 {
     int i;
@@ -212,6 +308,13 @@ int maxWidthRetroLetter(const wchar_t *retroLetter)
     return maxLineLength;
 }
 
+/**
+ * @brief Converts ASCII Letter to retrostyled letter
+ * 
+ * @param ptrLetter Retrostyled letter
+ * @param letter ASCII letter
+ * @return Returns the retrostyled letter chosen by ASCII letter
+ */
 const wchar_t* convertToRetroLetter(const wchar_t *ptrLetter, char letter)
 {
     switch(letter)
